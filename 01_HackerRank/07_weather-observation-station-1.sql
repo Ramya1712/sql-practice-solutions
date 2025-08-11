f382869253e21ffd221203d6c262c91c3b71e2f4
Ramya1712 <59701511+Ramya1712@users.noreply.github.com>
2025-08-11 13:49:51 +0530

    Create 07_weather-observation-station-1.sql

---
+++ 01_HackerRank/07_weather-observation-station-1.sql
@@ -0,0 +1,9 @@
+-- Problem : Query a list of CITY and STATE from the STATION table.
+-- Source: https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=false
+-- Difficulty: Easy
+
+-- 📝 Explanation:
+-- Select city and state column from station table.
+-- No filter conditions
+
+select city,state from station;
