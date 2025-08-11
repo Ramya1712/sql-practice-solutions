e42926a14423fce5b10682dbf1498e65e460bfd5
Ramya1712 <59701511+Ramya1712@users.noreply.github.com>
2025-08-11 13:39:05 +0530

    Create 04_select_by_id.sql

---
+++ 01_HackerRank/04_select_by_id.sql
@@ -0,0 +1,8 @@
+-- Problem: Select All
+-- Source: https://www.hackerrank.com/challenges/select-all/problem
+-- Difficulty: Easy
+
+-- 📝 Explanation:
+-- 1. The task is to retrieve all columns from the CITY table for the id = 1661.
+
+select * from city where id=1661;
