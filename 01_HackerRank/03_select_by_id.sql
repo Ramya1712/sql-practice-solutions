719f79d92c0beaf5650b4303968d69d460318e4b
Ramya1712 <59701511+Ramya1712@users.noreply.github.com>
2025-08-11 13:38:42 +0530

    Delete 01_HackerRank/03_select_by_id.sql

--- 01_HackerRank/03_select_by_id.sql
+++
@@ -1,8 +0,0 @@
--- Problem: Select All
--- Source: https://www.hackerrank.com/challenges/select-all/problem
--- Difficulty: Easy
-
--- 📝 Explanation:
--- 1. The task is to retrieve all columns from the CITY table for the id = 1661.
-
-select * from city where id=1661;
