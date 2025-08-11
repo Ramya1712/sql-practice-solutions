ed24503d9976959e7ff8c021acf995a85293e707
Ramya1712 <59701511+Ramya1712@users.noreply.github.com>
2025-08-11 13:41:40 +0530

    Create 05_japanese-cities-attributes.sql

---
+++ 01_HackerRank/05_japanese-cities-attributes.sql
@@ -0,0 +1,11 @@
+-- Problem : Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+-- Source: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=false
+-- Difficulty: Easy
+
+-- 📝 Explanation:
+-- Select Name column from CITY table where COUNTRYCODE is 'JPN'
+
+
+
+select * from city 
+where countrycode = 'JPN';
