4f49be561285b608fa4477376f4f16484bd90e1d
Ramya1712 <59701511+Ramya1712@users.noreply.github.com>
2025-08-11 13:35:34 +0530

    Create 03_Select_all.sql

---
+++ 01_HackerRank/03_Select_all.sql
@@ -0,0 +1,9 @@
+-- Problem: Select All
+-- Source: https://www.hackerrank.com/challenges/select-all/problem
+-- Difficulty: Easy
+
+-- 📝 Explanation:
+-- 1. The task is to retrieve all columns from the CITY table.
+-- 2. No filtering needed.
+
+select * from city;
